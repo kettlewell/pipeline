bdb21eea9898c0c483f666abfc3ebca88ecbde15
Matt Kettlewell <matt@kettlewell.net>
2017-05-24 16:48:45 -0500

    angular2 cleanup...

--- Display/angular2/src/app/app.component.js
+++ Display/angular2/src/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1><p>I Love You!</p>",
+        template: "<h1>Hello {{name}}</h1><p>Last comment removed ... oh my... you can see me!! </p>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- Display/angular2/src/app/app.component.ts
+++ Display/angular2/src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1><p>I Love You!</p>`,
+  template: `<h1>Hello {{name}}</h1><p>Last comment removed ... oh my... you can see me!! </p>`,
 })
 export class AppComponent  { name = 'Angular'; }
